e1b4ae91e842cc8e840c123d765279af5176fa11
dsArnab <dasarnab205@gmail.com>
2025-05-30 13:32:23 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require ('express');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+const {v4: uuidv4} = require('uuid');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+const loadUsers = () =>{
+    if (!fstat.existsSync('users.json')){
+        fstat.writeFileSync('users.json', JSON.stringify([]));
+    }
+    const data = fs.readFileSync('users.json');
+    return JSON.parse(data);
+}
+const isValidEmail = (email) => /^[^\$@]+@[^\$@]+\.[^\$@]+$/.test(email);
+
+app.post('/register', async (req, res) => {
+    const {username, email, password} = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({message: 'All the fields are required'});
+    }
+    if (!isValidEmail(email)){
+        return res.status(400).json({message: 'Invalid email format'});
+    }
+    if (password.length <8) {
+        return res.status(400).json({message: 'Password must be at least 8 characters'});
+    }
+
+    const users = loadUsers();
+
+    const existingUser = users.find (u => u.email === email || u.username === username);
+    if (existingUser){
+        return res.status(409).json({message: 'User already exists'});
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const newUser = {
+        id: uuidv4(),
+        username,
+        email,
+        password: hashedPassword
+    };
+
+    users.push(newUser);
+    saveUsers(users);
+
+    res.status(201).json({
+        userId: newUser.id,
+        message: 'User registration is successful'
+    });
+});
+
+app.listen(PORT, () =>{
+    console.log ('Server is running at http://localhost:8080');
+});
